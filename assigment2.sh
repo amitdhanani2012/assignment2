4ec8015c177b1273c973a57456bb3005ff8ec05c
root <amit.dhanani@yahoo.com>
2014-07-22 17:20:24 +0530

    Modified assigment.sh with fixed bugs

--- assigment2.sh
+++ assigment2.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-dpkg -l | grep -i php5-fpm || apt-get --force-yes install php5-fpm
-dpkg -l | grep -i mysql-server || apt-get --force-yes  install mysql-server
-dpkg -l | grep -i php5-mysql || apt-get --force-yes install php5-mysql
-sudo mysql_install_db
-dpkg -l | grep -i nginx || $(apt-get remove --force-yes nginx;apt-get install nginx )
+echo "Pre-requiste is this script should run as root. If you are root user then please enter to continue"
+read rt
+sudo add-apt-repository ppa:ondrej/php5
+apt-get update 
+dpkg -l | grep -i php5-fpm || apt-get --force-yes --fix-missing install php5-fpm
+dpkg -l | grep -i mysql-server || apt-get --force-yes --fix-missing install mysql-server
+dpkg -l | grep -i php5-mysql || apt-get --force-yes --fix-missing install php5-mysql
+mysql_install_db
+dpkg -l | grep -i nginx || $(apt-get remove --force-yes nginx;apt-get --force-yes --fix-missing install  nginx )
 grep -v "cgi.fix_path=0" /etc/php5/fpm/php.ini > /tmp/php.ini
 echo "cgi.fix_path=1" >> /tmp/php.ini
 cp -f /tmp/php.ini /etc/php5/fpm/php.ini
@@ -20,11 +23,14 @@ echo "Please enter IP for given domain name: "
 read ip
 
 echo "$ip $dm">> /etc/hosts
-touch /etc/nginx/site-available/$dm
-ln -s /etc/nginx/site-available/$dm /etc/nginx/site-enabled/$dm
+grep -v "bind-address" /etc/mysql/my.cnf > /tmp/my.cnf
+cp /tmp/my.cnf /etc/mysql/my.cnf
+service mysql restart
+touch /etc/nginx/sites-available/$dm
+ln -s /etc/nginx/sites-available/$dm /etc/nginx/sites-enabled/$dm
 mkdir -p /var/www/html/$dm/wordpress
 chown www-data.www-data /var/www/html/$dm/wordpress/
-cat domain-file<<end
+cat > /tmp/domain-file<<end
 server {
 	listen $ip:80;
 
@@ -49,44 +55,51 @@ server {
 }	
 end
 
-cat domain-file >  /etc/nginx/site-available/$dm
+cat /tmp/domain-file >  /etc/nginx/sites-available/$dm
 chown -R www-data.www-data /var/www/html/$dm
-
+rm  /tmp/domain-file
 service nginx restart
-cd /var/www/html/$dm/wordpress
+cd /var/www/html/$dm/
+rm latest.tar.gz
 wget https://wordpress.org/latest.tar.gz 
 tar -xzvf latest.tar.gz
+cd -
 echo "Enter username for wordpress "
 read ur
-echo " Enter password for wordpress "
-read ps;
-echo "Enter password for database "
+echo " Enter password for wordpress user $ur"
+read ps
+echo " Enter password for wordpress database"
 read dps
 echo "Enter your email address "
 read email
+echo "Enter username of mysql server"
+read u
+echo "Enter password of mysql server"
+read p
 cat > grant.sql<<end
-create user 'wordpress"@'$dm' identified by '$ps';
+use mysql;
+create user 'wordpress'@'$dm' identified by '$dps';
 grant all on wordpress.* to 'wordpress'@'$dm';
 end
-mysql < grant.sql 
-mysql < wordpress.sql
+mysql -u $u -p$p < grant.sql 
+mysql --host=$dm -u wordpress  -p$dps < wordpress.sql
 year=`date +%Y`
 month=`date +%m`
 day=`date +%d`
 time=`date +%H:%M:%S`
 
-cat > wordpress2.sql<<end
+cat > /tmp/wordpress2.sql<<end
 use wordpress;
-delete * from wp_users;
+delete  from wp_users;
 insert into wp_users values('','$ur',password('$ps'),'$ur','$email','',' '$year-$month-$day $time','',0,'$ur');
 end 
-mysql -u wordpress -p$ps < wordpress2.sql
-
-cat > wp-config.php<<end
+mysql --host=$dm -u wordpress -p$dps  < /tmp/wordpress2.sql
+touch /tmp/wp-config.php
+sudo cat > /tmp/wp-config.php<<end
 <?php
 define('DB_NAME', 'wordpress');
-define('DB_USER', '$ur');
-define('DB_PASSWORD', '$ps');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', '$dps');
 define('DB_HOST', '$dm');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
@@ -98,17 +111,18 @@ define('AUTH_SALT',        'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
-$table_prefix  = 'wp_';
+\$table_prefix  = 'wp_';
 define('WPLANG', '');
 define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
         define('ABSPATH', dirname(__FILE__) . '/');
 require_once(ABSPATH . 'wp-settings.php');
 end
-cp wp-config.php /var/www/html/$dm/wordpress/.
+
+sudo cp /tmp/wp-config.php /var/www/html/$dm/wordpress/.
 chown -R www-data.www-data /var/www/html/$dm/wordpress/
-rm wp-config.php
-rm wordpress2.sql
+rm /tmp/wp-config.php
+rm /tmp/wordpress2.sql
 rm grant.sql
 
 
